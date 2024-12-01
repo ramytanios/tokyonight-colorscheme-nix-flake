247a64047d78cf11bb164a6bbc881f1683209336
efettf <efettf@proton.me>
2024-12-01 17:53:39 +0100

    merge flake inputs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,8 @@
 {
   description = "Nix flake for the popular `tokyonight` colorscheme";
-  inputs.tokyonight = {
-    type = "github";
-    owner = "folke";
-    repo = "tokyonight.nvim";
-    flake = false;
-  };
+
+  inputs.tokyonight.url = "github:folke/tokyonight.nvim";
+  inputs.tokyonight.flake = false;
 
   outputs = inputs@{ self, ...}: {
     homeModules = { colorscheme = import ./colorscheme inputs; };
